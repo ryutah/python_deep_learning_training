6acfc6e500ee83445645f861054b957856f34b4b
ryuta <ryuta0221@gmail.com>
2017-09-09 23:20:25 +0900

    Update: レイヤクラス追加

--- layer.py
+++ layer.py
@@ -1,4 +1,7 @@
-class MulLayer:
+import numpy as np
+
+
+class MulLayer(object):
     """
     乗算レイヤ
     """
@@ -25,7 +28,7 @@ class MulLayer:
         return dx, dy
 
 
-class AddLayer:
+class AddLayer(object):
     """
     加算レイヤ
     """
@@ -43,3 +46,63 @@ class AddLayer:
         dx = dout * 1
         dy = dout * 1
         return dx, dy
+
+
+class Relu(object):
+    """
+    活性化関数として使われるReLU(Rectified Linear Unit)レイヤ
+    """
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy
+        out[self.mask] = 0
+        return out
+
+    def backwark(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+
+class Sigmoid(object):
+    """
+    活性化関数として使われるSigmoidレイヤ
+    """
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / 1 + np.exp(-x)
+        self.out = out
+        return out
+
+    def backwark(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+
+class Affine(object):
+    """
+    ニューララルネットワークの順伝搬で行う行列の積(アフィン変換)
+    のレイヤ
+    """
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        return out
+
+    def backwark(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        return dx
